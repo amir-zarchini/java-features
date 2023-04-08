cdeedee5d7bd24b8bc4523293c4ff4ae7a744e00
amir-zarchinie <amzarchini@gmail.com>
2023-04-08 11:09:30 +0330

    edit ep1

--- lambda-expressions-ep1/src/com/company/Main.java
+++ lambda-expressions-ep1/src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    StringToInt stringToInt = (String strs) -> strs.length();
+        int length = stringToInt.map("test lambda");
+        System.out.println(length);
+    }
+
+    interface StringToInt {
+        int map(String str);
     }
 }
