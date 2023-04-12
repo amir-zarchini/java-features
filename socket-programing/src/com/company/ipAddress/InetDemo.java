01882537d0082dea61de20569d4ece5916d2dd73
Amir_zr <amzarchini@gmail.com>
2023-04-12 23:46:02 +0330

    add example : InetAddress class

---
+++ socket-programing/src/com/company/ipAddress/InetDemo.java
@@ -0,0 +1,13 @@
+package com.company.ipAddress;
+
+import java.net.InetAddress;
+
+public class InetDemo{
+    public static void main(String[] args){
+        try{
+            InetAddress ip=InetAddress.getByName("195.201.10.8");
+
+            System.out.println("Host Name: "+ip.getHostName());
+        }catch(Exception e){System.out.println(e);}
+    }
+}
--- socket-programing/src/com/company/socket/Client.java
+++ socket-programing/src/com/company/socket/Client.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.socket;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
